b8c38bb44f36cb7f7cc6a1c7a1366a20e5db8413
vasantha kumar <kvasanth373@gmail.com>
2020-11-27 02:59:57 +0530

    video upload notification and progress bar added

--- src/components/imports/VideoUpload.js
+++ src/components/imports/VideoUpload.js
@@ -5,12 +5,19 @@ import { Form, Button, Segment, Loader } from "semantic-ui-react"
 import { videoActions } from "../../actions/videoActions"
 import { appHelperFunctions } from "../../helpers/appHelper"
 import Typography from "@material-ui/core/Typography"
-import SignIn from "./SignIn/SignIn"
+import LinearProgress from "@material-ui/core/LinearProgress"
+import Snackbar from "@material-ui/core/Snackbar"
+import Alert from "@material-ui/lab/Alert"
 import { withRouter } from "react-router-dom"
 
 const VideoUpload = (props) => {
    const [videoTitle, setVideoTitle] = useState(props.title)
    const [isUploading, setIsUploading] = useState(false)
+   const [isErrorPublishing, setIsErrorPublishing] = useState({
+      isToast: false,
+      isError: false,
+      msg: "",
+   })
    const appState = useSelector((state) => state.video)
    const dispatch = useDispatch()
    const isWeb3Available = useSelector((state) => state.user.isWeb3Available)
@@ -43,7 +50,12 @@ const VideoUpload = (props) => {
                   type="text"
                   className="form-control-sm"
                   placeholder="Title.."
-                  onChange={(e) => setVideoTitle(e.target.value)}
+                  onChange={(e) => {
+                     setVideoTitle(e.target.value)
+                     dispatch(
+                        videoActions.setData("currentTitle", e.target.value)
+                     )
+                  }}
                   required
                />
             </Form.Field>
@@ -57,11 +69,7 @@ const VideoUpload = (props) => {
                      const file = e.target.files[0]
                      const reader = new window.FileReader()
                      reader.readAsArrayBuffer(file)
-
                      reader.onloadend = () => {
-                        dispatch(
-                           videoActions.setData("currentTitle", videoTitle)
-                        )
                         dispatch(
                            videoActions.setData("buffer", Buffer(reader.result))
                         )
@@ -70,34 +78,109 @@ const VideoUpload = (props) => {
                />
             </Form.Field>
 
+            {isUploading ? <LinearProgress variant="indeterminate" /> : null}
+
             <Button
                type="submit"
+               style={{ marginTop: "2%" }}
                onClick={() => {
-                  console.log("Submitting to IPFS....")
+                  // let data = {
+                  //    a: "123",
+                  //    b: "test",
+                  // }
 
-                  const ipfs = appHelperFunctions.getIPFSClient()
-                  //Add to IPFS
-                  ipfs.add(appState.buffer, (err, result) => {
-                     console.log("IPFS result", result)
-                     if (err) {
-                        console.error(err)
-                        return
-                     }
+                  // ipfs.add(Buffer.from(JSON.stringify(data)))
+                  //    .then((res) => {
+                  //       const hash = res[0].hash
+                  //       console.log("added data hash:", hash)
+                  //       return ipfs.cat(hash)
+                  //    })
+                  //    .then((output) => {
+                  //       console.log("retrieved data:", JSON.parse(output))
+                  //    })
+
+                  // Add to IPFS
+                  if (appState.buffer && appState.currentTitle) {
+                     console.log("Submitting to IPFS....")
+
+                     const ipfs = appHelperFunctions.getIPFSClient()
                      setIsUploading(true)
-                     // Put on blockchain
-                     appState.dvideo.methods
-                        .uploadVideo(result[0].hash, appState.currentTitle)
-                        .send({ from: account })
-                        .on("transactionHash", (hash) => {
-                           setIsUploading(false)
-                        })
-                  })
+                     ipfs.add(appState.buffer, (err, result) => {
+                        console.log("IPFS result", result)
+                        setIsUploading(false)
+                        if (err) {
+                           setIsUploading(true)
+                           console.error(err)
+                           return
+                        }
+
+                        if (appState.currentTitle && result[0].hash) {
+                           // Put on blockchain
+                           appState.dvideo.methods
+                              .uploadVideo(
+                                 result[0].hash,
+                                 appState.currentTitle
+                              )
+                              .send({ from: account })
+                              .on("transactionHash", (hash) => {
+                                 setIsUploading(false)
+                                 setIsErrorPublishing({
+                                    isToast: true,
+                                    isError: false,
+                                    msg: "Video publised - Successfully ",
+                                 })
+                              })
+                              .catch((err) => {
+                                 console.log(err)
+                                 setIsErrorPublishing({
+                                    isToast: true,
+                                    isError: true,
+                                    msg:
+                                       "Video not publised - Transaction failed! ",
+                                 })
+                              })
+                        } else {
+                           setIsErrorPublishing({
+                              isToast: true,
+                              isError: true,
+                              msg: "Fill all the details ",
+                           })
+                        }
+                     })
+                  } else {
+                     setIsErrorPublishing({
+                        isToast: true,
+                        isError: true,
+                        msg: "Fill all the details ",
+                     })
+                  }
                }}
             >
                Submit
             </Button>
-            <Loader active={isUploading}>uploading...</Loader>
          </Form>
+         <Snackbar
+            open={isErrorPublishing.isToast}
+            autoHideDuration={6000}
+            onClose={() =>
+               setIsErrorPublishing({ isToast: false, isError: true, msg: "" })
+            }
+         >
+            <Alert
+               elevation={6}
+               variant="filled"
+               onClose={() =>
+                  setIsErrorPublishing({
+                     isToast: false,
+                     isError: true,
+                     msg: "",
+                  })
+               }
+               severity={isErrorPublishing.isError ? "error" : "success"}
+            >
+               {isErrorPublishing.msg}
+            </Alert>
+         </Snackbar>
       </Container>
    )
 }
